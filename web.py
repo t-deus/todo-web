7a46ff01419ddc6c9a68e14f5be49b092a312c94
t-deus <139822387+t-deus@users.noreply.github.com>
2024-05-09 09:10:13 -0500

    Initial commit

---
+++ web.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title("Today's Todos")
+st.subheader("Keep track of your day.")
+st.write("Keep your head up")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="", placeholder="Enter a todo",
+              on_change=add_todo, key='new_todo')
